ecad347b98ae821b9a744f60991234195194d74c
nielh <nielh@DESKTOP-NQ60TR7>
2017-06-26 23:53:00 -0500

    Nielsen. Creación de la Conexión a la Base de Datos.

---
+++ ProyectoFarmacia/src/modelo/Conexion.java
@@ -0,0 +1,26 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+	//Campos o atributos
+	private String Driver = "com.mysql.jdbc.Driver";
+	private String URL = "jdbc:mysql://localhost:3306/bd_farmacia"; //Conexión BD_Farmacia
+	private String User = "root";
+	private String Password = "";
+	//Constructor
+	public Conexion() {
+	}
+	//Métodos
+	public Connection Conectar(){
+		Connection cn = null;
+		try {
+			Class.forName(Driver);
+			cn = DriverManager.getConnection(URL,User,Password);
+		} catch (Exception e) {
+			System.out.println("Error al conectar BD.\n"+e.getMessage());
+		}
+		return cn;
+	}
+}
